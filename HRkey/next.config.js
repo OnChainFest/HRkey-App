decccc72b1955be99cd697666167fb0f549f0c1f
vicvalch <vicvalch@gmail.com>
2025-11-08 01:00:27 -0600

    fix: add rewrites for html routes

--- HRkey/next.config.js
+++ HRkey/next.config.js
@@ -6,6 +6,34 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/auth',
+        destination: '/auth.html'
+      },
+      {
+        source: '/app',
+        destination: '/app.html'
+      },
+      {
+        source: '/request-reference',
+        destination: '/request-reference.html'
+      },
+      {
+        source: '/reference-management-page',
+        destination: '/reference-management-page.html'
+      },
+      {
+        source: '/referee-evaluation-page',
+        destination: '/referee-evaluation-page.html'
+      },
+      {
+        source: '/pricing',
+        destination: '/pricing.html'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
