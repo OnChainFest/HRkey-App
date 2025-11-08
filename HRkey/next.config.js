d612224344c7667bd174342a125cd17429ca617b
vicvalch <36937201+vicvalch@users.noreply.github.com>
2025-11-07 22:44:24 -0600

    Update next.config.js

--- HRkey/next.config.js
+++ HRkey/next.config.js
@@ -2,11 +2,9 @@
 const nextConfig = {
   async rewrites() {
     return [
-      // Asegura que /WebDapp/* se sirva tal cual desde /public
       { source: '/WebDapp/:path*', destination: '/WebDapp/:path*' },
-    ];
+    ]
   },
-  // Opcional, por si hay confusiones con /ruta/ vs /ruta
   trailingSlash: false,
-};
-module.exports = nextConfig;
+}
+module.exports = nextConfig
