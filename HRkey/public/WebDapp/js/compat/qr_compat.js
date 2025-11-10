d57ca03a1971c00ef923f2377cf13c7de901ffd7
vicvalch <36937201+vicvalch@users.noreply.github.com>
2025-11-10 16:19:19 -0600

    Delete HRkey/public/WebDapp/js/compat/qr_compat.js

--- HRkey/public/WebDapp/js/compat/qr_compat.js
+++
@@ -1,7 +0,0 @@
-(function(){
-  function getParam(n){ try{ return new URL(location.href).searchParams.get(n); }catch(e){ return null; } }
-  window.HRKEY_REF_TOKEN = getParam('token') || getParam('ref') || getParam('id') || '';
-  // Opcional: expón BASE_URL consistente
-  var isLocal = (location.hostname==='localhost'||location.hostname==='127.0.0.1');
-  window.HRKEY_BASE_URL = isLocal ? 'https://hrkey.xyz' : location.origin;
-})();
