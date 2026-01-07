1f2a000103a2f60fe1a68c231c3038630021f1a9
vicvalch <36937201+vicvalch@users.noreply.github.com>
2026-01-07 13:56:49 -0600

    Add Next.js configuration in next.config.mjs

---
+++ HRkey/next.config.mjs
@@ -0,0 +1,17 @@
+// next.config.mjs
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  turbopack: {
+    root: __dirname,
+  },
+};
+
+export default nextConfig;
